7dc1dbe0d054f4269eac99de0f5f81eb0f2b5410
Ryan <ryankilbane92@hotmail.com>
2021-05-06 12:56:30 +0100

    Fixed some import issues, added unit test around adding new order. Found a couple of issues where I'd missed adding values in the builder

---
+++ tests/test_process.py
@@ -0,0 +1,22 @@
+from order_book.process import make_order
+from order_book.order.order import Order, OrderBuilder
+
+def test_add_order():
+    incoming = "123456789|abbb111|a|AAPL|B|209.00000|100"
+    dummy_builder = OrderBuilder()
+    order = Order(dummy_builder)
+    order.time_stamp = "123456789"
+    order.order_id = "abbb111"
+    order.action = "a"
+    order.ticker = "AAPL"
+    order.side = "B"
+    order.price = 209.0
+    order.size = 100
+    new_order = make_order(incoming)
+    assert new_order.time_stamp == order.time_stamp
+    assert new_order.order_id == order.order_id
+    assert new_order.action == order.action
+    assert new_order.ticker == order.ticker
+    assert new_order.side == order.side
+    assert new_order.price == order.price
+    assert new_order.size == order.size
