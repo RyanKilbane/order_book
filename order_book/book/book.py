e9a6f7dcf9612edb7cfbfb6dc43b788d937f8a53
Ryan <ryankilbane92@hotmail.com>
2021-05-06 20:37:48 +0100

    Moved iterators into book_itertors

--- order_book/book/book.py
+++ order_book/book/book.py
@@ -2,6 +2,8 @@ from abc import ABC, abstractmethod
 from typing import Dict, Union
 from enum import Enum
 
+from order_book.book.book_iterators import PriceBinarySearch, OrderIdLinearSearch
+
 class Book(ABC):
     @abstractmethod
     def find_by():
@@ -10,52 +12,6 @@ class Book(ABC):
     def insert():
         pass
 
-
-class OrderBookIterator(ABC):
-    @abstractmethod
-    def iterate():
-        pass
-
-
-class PriceBinarySearch(OrderBookIterator):
-    def __init__(self, orders):
-        self.orders = orders
-
-    def _find_max(self, buy_orders):
-        if buy_orders is None:
-            return -float("inf")
-        left_max = self._find_max(buy_orders.left)
-        right_max = self._find_max(buy_orders.right)
-        if left_max >= buy_orders:
-            return left_max
-        elif right_max >= buy_orders:
-            return right_max
-        return buy_orders
-
-
-    def _find_min(self, ask_orders):
-        if ask_orders is None:
-            return float("inf")
-        left_min = self._find_min(ask_orders.left)
-        right_min = self._find_max(ask_orders.right)
-        if left_min <= ask_orders:
-            return left_min
-        elif right_min <= ask_orders:
-            return right_min
-        return ask_orders
-
-    def iterate(self):
-        max_buy = self._find_max(self.orders["B"].root)
-        min_ask = self._find_min(self.orders["S"].root)
-        return max_buy, min_ask
-
-
-class OrderIdLinearSearch(OrderBookIterator):
-    def __init__(self, orders):
-        self.orders = orders
-
-    def iterate(self):
-        pass
 
 class OrderBook(Book):
     def __init__(self):
