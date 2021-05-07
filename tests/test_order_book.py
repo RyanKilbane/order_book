0f01c9d7dbf592e4c50a2b750fc9bd2366c41795
Ryan <ryankilbane92@hotmail.com>
2021-05-07 10:51:59 +0100

    added unit test around exception raising

--- tests/test_order_book.py
+++ tests/test_order_book.py
@@ -1,6 +1,8 @@
+from pytest import raises
 from order_book.process import make_order
 from order_book.order.order import Order, OrderBuilder
 from order_book.book.book import OrderBook, TickerOrderBook
+from order_book.book.exceptions import NoTickerException
 
 def test_add_new_order_book():
     order_book = OrderBook()
@@ -9,3 +11,8 @@ def test_add_new_order_book():
     order_book.insert(order)
     order_book.insert(order)
     order_book.insert(order)
+
+def test_no_ticker_found():
+    order_book = OrderBook()
+    with raises(NoTickerException) as e:
+        order_book["AAPL"]
